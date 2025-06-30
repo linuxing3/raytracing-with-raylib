491d20d9b074b03799f3c35f1f95c8f5efa736fe
wenju xing <linuxing3@qq.com>
2025-06-30 14:30:39 -0300

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+        devenv.shells.default = {
+
+          languages.c.enable = true;
+
+          enterShell = ''
+              cmake -S . -B build
+              cmake  --build build
+          '';
+        };
+      };
+    };
+}
